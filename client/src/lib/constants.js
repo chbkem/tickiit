dc412768d68f11713bc1e8a4c81a9d014b58975c
chibuokem <chibuokemuwaechina@gmail.com>
2026-09-19 00:43:41 +0100

    updated API URL

--- client/src/lib/constants.js
+++ client/src/lib/constants.js
@@ -1,4 +1,4 @@
-export const API_BASE_URL = 'http://localhost:5000';
+export const API_BASE_URL = 'tickiit-production.up.railway.app';
 
 export const PRIORITY_OPTIONS = [
   { value: 'low', label: 'Low' },
